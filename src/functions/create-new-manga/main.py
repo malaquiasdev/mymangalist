7265e274e6661fb584986d146deeaebcafe3fa01
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-12 15:33:08 -0300

    set a status code 409 if the manga exist in my list as rule

--- src/functions/create-new-manga/main.py
+++ src/functions/create-new-manga/main.py
@@ -9,13 +9,26 @@ DYNAMODB_MANGAS_TABLE_NAME = os.getenv('DYNAMODB_MANGAS_TABLE_NAME')
 def handler(event, context):
     print(f'DYNAMODB_MANGAS_TABLE_NAME: {DYNAMODB_MANGAS_TABLE_NAME}')
     print(f'event: {event}')
+
     request_body = {**event["body"]}
-    exist_in_my_list = dynamodb.find_manga_by_slug(DYNAMODB_MANGAS_TABLE_NAME, request_body.get('title'))
+
+    exist_in_my_list = dynamodb.find_manga_by_slug(DYNAMODB_MANGAS_TABLE_NAME, request_body.get('slug'))
+
     if not exist_in_my_list:
         dynamodb.create_new_manga(DYNAMODB_MANGAS_TABLE_NAME, request_body.get('slug'), request_body.get('title'),
                                   request_body.get('status'), request_body.get('mangaStatus'),
                                   request_body.get('numChaptersRead'), request_body.get('averageScore'),
                                   request_body.get('scores'), request_body.get('externalLinks'))
+    else:
+        return {
+            "statusCode": 409,
+            "body": json.dumps({
+                "code": "CONFLICT_ERROR",
+                "message": f"{request_body.get('slug')} exist in your list",
+                'requestId': context.aws_request_id
+            })
+        }
+
     result = {
         'data': {
             'contents': '',
