691b58593eb5e8f237dbfc79636771683430d476
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-06-15 22:34:58 -0300

    Feat list mangas scores (#3)
    * create a new endpoint that return the scores of a manga, to help me with the apple shortcuts
    
    * fix a error in find_manga_by_slug to return the first item of the response when this is valid

---
+++ src/functions/listMyMangasScores/main.py
@@ -0,0 +1,20 @@
+import os
+
+from src.functions.listMyMangasScores.service import list_my_manga_scores
+from src.utils.http.response import make_response
+
+DYNAMODB_MANGAS_TABLE_NAME = os.getenv('DYNAMODB_MANGAS_TABLE_NAME')
+
+
+def handler(event, context):
+    print(f'DYNAMODB_MANGAS_TABLE_NAME: {DYNAMODB_MANGAS_TABLE_NAME}')
+    print(f'event: {event}')
+    try:
+        request_path = event['pathParameters']
+        my_manga_scores = list_my_manga_scores(DYNAMODB_MANGAS_TABLE_NAME, request_path['slug'])
+        result = {
+            "scores": my_manga_scores
+        }
+        return make_response(context.aws_request_id, 0, 200, result)
+    except Exception as error:
+        return make_response(context.aws_request_id, 0, 500, None, "INTERNAL_SERVER_ERROR", str(error))
---
+++ src/functions/listMyMangasScores/service.py
@@ -0,0 +1,6 @@
+from src.components.dynamodb import dynamodb
+
+
+def list_my_manga_scores(table_name: str, slug: str):
+    manga = dynamodb.find_manga_by_slug(table_name, slug)
+    return manga["scores"]
