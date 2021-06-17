f86d911bee17b6d39e7a1980f7bc732c71dfe43f
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-16 23:39:00 -0300

    fix the logic to update the score and fix the request.json validate

--- src/functions/addNewScore/service.py
+++ src/functions/addNewScore/service.py
@@ -1,9 +1,12 @@
 from src.components.dynamodb import dynamodb
 
 
-def add_new_score(table_name: str, slug: str, score: dict):
+def add_new_score(table_name: str, slug: str, new_score: dict):
     manga = dynamodb.find_manga_by_slug(table_name, slug)
     if not manga:
         return None
-    manga["scores"].append(score)
+    for index, score in enumerate(manga['scores']):
+        if score['chapter_number'] == new_score['chapter_number']:
+            manga['scores'].pop(index)
+    manga["scores"].append(new_score)
     return dynamodb.update_item(table_name, manga)
