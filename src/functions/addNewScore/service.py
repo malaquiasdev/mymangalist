9a4a653ae511ce530dfadafc4e2f770f3ac591c3
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-18 08:26:59 -0300

    feat - update num_chapters_read and average_score when a new score is added to the manga

--- src/functions/addNewScore/service.py
+++ src/functions/addNewScore/service.py
@@ -1,4 +1,5 @@
 from src.components.dynamodb import dynamodb
+from src.utils.math.calculate_average_score import calculate_average_score
 
 
 def add_new_score(table_name: str, slug: str, new_score: dict):
@@ -8,5 +9,7 @@ def add_new_score(table_name: str, slug: str, new_score: dict):
     for index, score in enumerate(manga['scores']):
         if score['chapter_number'] == new_score['chapter_number']:
             manga['scores'].pop(index)
+    manga["num_chapters_read"] = new_score["chapter_number"]
     manga["scores"].append(new_score)
+    manga["average_score"] = calculate_average_score(manga["scores"])
     return dynamodb.update_item(table_name, manga)
