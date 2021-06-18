06a117f1e3d434a4d8388c4e2fe08ed672a91286
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-18 08:28:18 -0300

    feat - add_new_score now returns False when a manga is not updated

--- src/functions/addNewScore/service.py
+++ src/functions/addNewScore/service.py
@@ -5,7 +5,7 @@ from src.utils.math.calculate_average_score import calculate_average_score
 def add_new_score(table_name: str, slug: str, new_score: dict):
     manga = dynamodb.find_manga_by_slug(table_name, slug)
     if not manga:
-        return None
+        return False
     for index, score in enumerate(manga['scores']):
         if score['chapter_number'] == new_score['chapter_number']:
             manga['scores'].pop(index)
