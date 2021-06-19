eb07cbae38a631a14294c8892b37ccef5ddb1947
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-06-19 19:35:24 -0300

    #4 - rouding the average value to one. So a decimal value will have always one value after the common (#5)

--- src/utils/math/calculate_average_score.py
+++ src/utils/math/calculate_average_score.py
@@ -1,9 +1,11 @@
 import typing
 
+ROUNDING_VALUE_TO_ONE = 1
+
 
 def calculate_average_score(scores: typing.List[dict]) -> float:
     total_score = 0
     for item in scores:
         total_score = total_score + item["score"]
 
-    return total_score / len(scores)
+    return round(total_score / len(scores), ROUNDING_VALUE_TO_ONE)
--- test/utils/math/test_calculate_average_score.py
+++ test/utils/math/test_calculate_average_score.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 from src.utils.math.calculate_average_score import calculate_average_score
 
 
@@ -32,3 +33,17 @@ class CalculateAverageScoreModuleTest(unittest.TestCase):
         ]
         average = calculate_average_score(scores)
         self.assertEqual(average, 5, "The average must be equal to 5")
+
+    def test_calculate_average_score_PI_value(self):
+        scores = [
+            {
+                "chapter_number": 1,
+                "score": math.pi,
+            },
+            {
+                "chapter_number": 2,
+                "score": 3,
+            }
+        ]
+        average = calculate_average_score(scores)
+        self.assertEqual(average, 3.1, "The average must be equal to 3.1")
