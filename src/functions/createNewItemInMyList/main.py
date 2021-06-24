a5a3328fc4b42db0552fbe9c54ce8b4b4f2934c1
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-23 22:27:24 -0300

    refactor the createNewItemInMyList to accept more items and change the gateway validation

--- src/functions/createNewItemInMyList/main.py
+++ src/functions/createNewItemInMyList/main.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from src.functions.createNewItemInMyList.service import create_new_manga_if_not_exist_in_my_list
+from src.functions.createNewItemInMyList.service import create_new_item_if_not_exist_in_my_list
 from src.utils.http.response import make_response
 
 DYNAMODB_MANGAS_TABLE_NAME = os.getenv('DYNAMODB_MANGAS_TABLE_NAME')
@@ -13,10 +13,9 @@ def handler(event, context):
     request_body = json.loads(event['body'])
 
     try:
-        create_new_manga_if_not_exist_in_my_list(DYNAMODB_MANGAS_TABLE_NAME, request_body.get('slug'),
-                                                 request_body.get('title'), request_body.get('status'),
-                                                 request_body.get('mangaStatus'), request_body.get('numChaptersRead'),
-                                                 request_body.get('scores'), request_body.get('externalLinks'))
+        create_new_item_if_not_exist_in_my_list(DYNAMODB_MANGAS_TABLE_NAME, request_body.get('slug'),
+                                                request_body.get('title'), request_body.get('status'),
+                                                request_body.get('type'))
     except ValueError as error:
         return make_response(context.aws_request_id, 0, 409, None, "CONFLICT_ERROR", str(error))
     except Exception as error:
--- src/functions/createNewItemInMyList/service.py
+++ src/functions/createNewItemInMyList/service.py
@@ -1,15 +1,10 @@
 from src.components.dynamodb import dynamodb
-from src.utils.math.calculate_average_score import calculate_average_score
 
 
-def create_new_manga_if_not_exist_in_my_list(table_name: str, slug: str, title: str, status: str, manga_status: str,
-                                             num_chapters_read: int, scores: object,
-                                             external_links: object) -> bool:
-    average_score = calculate_average_score(scores)
-    exist_in_my_list = dynamodb.find_manga_by_slug(table_name, slug)
+def create_new_item_if_not_exist_in_my_list(table_name: str, slug: str, title: str, status: str, type: str) -> bool:
+    exist_in_my_list = dynamodb.find_item_by_slug(table_name, slug)
     if not exist_in_my_list:
-        dynamodb.create_new_manga(table_name, slug, title, status, manga_status, num_chapters_read, average_score,
-                                  scores, external_links)
+        dynamodb.create_new_item(table_name, slug, title, status, type)
     else:
         raise ValueError(f"slug: {slug}, exist in your list")
 
