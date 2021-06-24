54336b36a87c742ab03637101d75c4915f07f752
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-23 22:04:20 -0300

    refactor the function name of createNewMangaInMyList to createNewItemInMyList

--- src/functions/createNewItemInMyList/main.py
+++ src/functions/createNewItemInMyList/main.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from src.functions.createNewManga.service import create_new_manga_if_not_exist_in_my_list
+from src.functions.createNewItemInMyList.service import create_new_manga_if_not_exist_in_my_list
 from src.utils.http.response import make_response
 
 DYNAMODB_MANGAS_TABLE_NAME = os.getenv('DYNAMODB_MANGAS_TABLE_NAME')
