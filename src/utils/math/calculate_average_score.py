6fb46d251531e9f7ee712ad1a64f8c12759ccdb4
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-06-27 10:58:29 -0300

    fix: calculate_average_score must return decimal type not float

--- src/utils/math/calculate_average_score.py
+++ src/utils/math/calculate_average_score.py
@@ -1,11 +1,12 @@
+from decimal import *
 import typing
 
 ROUNDING_VALUE_TO_ONE = 1
 
 
-def calculate_average_score(scores: typing.List[dict]) -> float:
+def calculate_average_score(scores: typing.List[dict]) -> Decimal:
     total_score = 0
     for item in scores:
         total_score = total_score + item["score"]
 
-    return round(total_score / len(scores), ROUNDING_VALUE_TO_ONE)
+    return Decimal(round(total_score / len(scores), ROUNDING_VALUE_TO_ONE))
